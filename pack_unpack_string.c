3fa50538dbcf0f17030ab1d8aab311b6de4f43ce
Kaya Sertel <50380412+KayaSrtl@users.noreply.github.com>
2023-05-22 19:57:03 +0300

    Update pack_unpack_string.c

--- pack_unpack_string.c
+++ pack_unpack_string.c
@@ -1,20 +1,92 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <conio.h>
-#define SIZE 100
 
-int main() {
-  char str1[] = "abcd", str2[] = "aabcds", str3[] = "abcd";
-  int result;
+int getDigitCount(int number) {
+    int digit_count = 0;
+    while(number) {
+        digit_count++;
+        number /= 10;
+    }
+    return digit_count;
+}
 
-  // comparing strings str1 and str2
-  result = strcmp(str1, str2+1);
-  printf("strcmp(str1, str2) = %d\n", result);
+int powerOfTenPositive(int num) {
+    int res = 1;
+    for(int i = 0; i < num; i++)
+        res *= 10;
+    return res;
+}
 
-  // comparing strings str1 and str3
-  result = strcmp(str1, str3);
-  printf("strcmp(str1, str3) = %d\n", result);
-while(!getch());
-  return 0;
+int getDigit(int num, int digit) {
+    int digit_count = getDigitCount(num);
+    num /= powerOfTenPositive(digit_count - digit);
+    num %= 10;
+    return num;
+}
+
+
+
+char* dataPacker(int* numData, int dataLength) {
+    static char stringData[150];
+    int temp, stringIndex = 0;
+    for(int i = 0; i < dataLength; i++) {
+        temp = getDigitCount(numData[i]);
+        for(int j = 1; j <= temp; j++) {
+            stringData[stringIndex] = getDigit(numData[i], j) + '0';
+            stringIndex++;
+        }
+        stringData[stringIndex] = '/';
+        stringIndex++;
+    }
+
+    stringData[stringIndex] = '\0';
+    return stringData;
+}
+
+int* dataUnPacker(char* stringData) {
+    int stringIndex = 0, temp = 0, intIndex = 0;
+    while(stringData[stringIndex] != '\0') {
+        while(stringData[stringIndex] != '/') {
+            
+            stringIndex++;
+        }
+            
+        stringIndex++;
+        intIndex++;
+    }
+    stringIndex = 0;
+    int *numData = (int *)malloc(intIndex * sizeof(int));
+    for(int i = 0; i < intIndex; i++)
+        numData[i] = 0;
+    intIndex = 0;
+    while(stringData[stringIndex] != '\0') {
+        while(stringData[stringIndex] != '/') {
+            stringIndex++;
+            temp++;
+        }
+        for(int i = temp-1; i >= 0; i--) {
+            numData[intIndex] += powerOfTenPositive(i)*(stringData[stringIndex - i - 1] - '0');
+        }
+        intIndex++;
+        stringIndex++;
+        temp = 0;
+    }
+    
+    
+    return numData;
+        
+}
+
+int main()
+{
+    int numData[13] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 3223};
+    int data_length = 13;
+    char* stringData = dataPacker(numData, data_length);
+    printf("%s", stringData);
+    int* num_data_res = dataUnPacker(stringData);
+    printf("\n");
+    for(int i = 0; i < data_length; i++)
+        printf("%d ", num_data_res[i]);
+    free(num_data_res);
+    return 0;
 }
